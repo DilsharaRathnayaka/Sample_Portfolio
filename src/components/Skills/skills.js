27d24f0f68bd56228ded73f95e094ef8c786196b
DilsharaRathnayaka <rmndrathnayaka@std.appsc.sab.ac.lk>
2024-09-02 10:18:39 +0530

    Update portfolio

---
+++ src/components/Skills/skills.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './skills.css';
+import UIDesign from '../../assets/ui-design.png';
+import WebDesign from '../../assets/website-design.png';
+import AppDesign from '../../assets/app-design.png';
+
+const Skills = () => {
+  return (
+    <section id='skills'>
+        <span className="skillTitle">What I do</span>
+        <span className="skillDesc">I am a skilled and passionate web designer with experience in creating visually appealing and user-friendly websites. I have a strong understanding of design and a keen eye for detail. I am proficient in HTML, CSS, react and JavaScript.</span>
+        <div className="skillBars">
+            <div className="skillBar">
+                <img src = {UIDesign} alt="UIDesign" className="skillBarImg" />
+                <div calssName="skillBarText">
+                    <h2>UI/UX Design</h2>
+                    <p>This is a demo text, you can write your own content here.</p>
+                </div>
+            </div>
+            <div className="skillBar">
+                <img src = {WebDesign} alt="WebDesign" className="skillBarImg" />
+                <div calssName="skillBarText">
+                    <h2>Web Design</h2>
+                    <p>This is a demo text, you can write your own content here.</p>
+                </div>
+            </div>
+            <div className="skillBar">
+                <img src = {AppDesign} alt="AppDesign" className="skillBarImg" />
+                <div calssName="skillBarText">
+                    <h2>App Design</h2>
+                    <p>This is a demo text, you can write your own content here.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Skills;
